b9619b2268ed93184dfb190e5f6852eee7a5b245
Phillip Ho <phillipho@hippo.local>
2020-03-26 21:59:40 -0700

    input steals focus on keypress

--- src/components/ChannelInput/index.js
+++ src/components/ChannelInput/index.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './ChannelInput.css';
 
 function ChannelInput({ channelName }) {
   const [message, setMessage] = useState('');
+  const [hasFocus, setHasFocus] = useState(false);
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function keyDownTakeFocus(event) {
+      if (!hasFocus) {
+        inputEl.current.focus();
+      }
+    }
+
+    document.addEventListener('keydown', keyDownTakeFocus);
+
+    return () => {
+      document.removeEventListener("keydown", keyDownTakeFocus);
+    }
+  });
 
   const handleMessage = (e) => {
     setMessage(e.target.value);
@@ -19,9 +35,12 @@ function ChannelInput({ channelName }) {
     <form className="channel-input" onSubmit={handleSubmit}>
       <input
         className="channel-input__input"
-        placeholder={`Message ${channelName}`}
-        value={message}
+        onBlur={() => setHasFocus(false)}
         onChange={handleMessage}
+        onFocus={() => setHasFocus(true)}
+        placeholder={`Message ${channelName}`}
+        ref={inputEl}
+        value={message}
       />
     </form>
     <div className="channel-input__tips"><strong>Return</strong> to send</div>
